c487111f9620bcde257aa9a9f9c714c6aa21f727
Aman Kaur <amankaur2628@gmail.com>
2020-06-09 19:08:52 -0400

    Map view added

--- FindWay_amanpreet_c0782918/ViewController.swift
+++ FindWay_amanpreet_c0782918/ViewController.swift
@@ -7,14 +7,15 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+      
     }
-
-
 }
 

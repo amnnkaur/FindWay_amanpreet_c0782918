c8804f6d7767f8fca02801e9cb098f7e19b39f3a
Aman Kaur <amankaur2628@gmail.com>
2020-06-09 18:23:22 -0400

    Initial Commit

---
+++ FindWay_amanpreet_c0782918/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FindWay_amanpreet_c0782918
+//
+//  Created by Aman Kaur on 2020-06-09.
+//  Copyright © 2020 Aman Kaur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

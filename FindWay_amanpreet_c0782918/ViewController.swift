a5b453432e1e219821f151209b170fcd83b7a081
Aman Kaur <amankaur2628@gmail.com>
2020-06-10 20:49:01 -0400

    live location found

--- FindWay_amanpreet_c0782918/ViewController.swift
+++ FindWay_amanpreet_c0782918/ViewController.swift
@@ -14,23 +14,44 @@ class ViewController: UIViewController, MKMapViewDelegate{
     @IBOutlet weak var mapView: MKMapView!
     @IBOutlet weak var findMyWayBtn: UIButton!
     @IBOutlet weak var segTransportType: UISegmentedControl!
+    @IBOutlet weak var stepZoom: UIStepper!
+    let locationManager: CLLocationManager = {
+       let manager = CLLocationManager()
+       manager.requestWhenInUseAuthorization()
+       return manager
+    }()
+       
+    var coordinate: CLLocationCoordinate2D?
+       
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.isZoomEnabled = false
+        mapView.showsUserLocation = true
         mapView.delegate = self
         
-        mapView.initialLocation(firstLocation)
+        mapView.showsCompass = true
+        mapView.showsScale = true
+        currentLocation()
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        }
+        
+//        mapView.initialLocation(firstLocation)
         let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap(recognizer:)))
         mapView.addGestureRecognizer(tap)
        
     }
     
-    let firstLocation = CLLocation(latitude: 43.683334, longitude: -79.766670)
+    
+//    let firstLocation = CLLocation(latitude: 43.683334, longitude: -79.766670)
     
     @IBAction func locationBtn(_ sender: UIButton) {
-        mapView.showsUserLocation = true
-       // getRoute(destination: )
+      
+        getRoute()
+      
     }
 
     @objc func handleTap(recognizer: UITapGestureRecognizer) {
@@ -50,40 +71,93 @@ class ViewController: UIViewController, MKMapViewDelegate{
              self.mapView.addAnnotation(annotation)
         }
     }
+    
+    func currentLocation() {
+       locationManager.delegate = self
+       locationManager.desiredAccuracy = kCLLocationAccuracyBest
+       if #available(iOS 11.0, *) {
+          locationManager.showsBackgroundLocationIndicator = true
+       } else {
+        
+       }
+       locationManager.startUpdatingLocation()
+    }
 
-    func getRoute(destination:CLLocationCoordinate2D) {
+    func getRoute() {
         
         let destCoordinate = MKDirections.Request()
         let sourceCoordinate = mapView.userLocation.coordinate
         
         let source = CLLocationCoordinate2DMake(sourceCoordinate.latitude, sourceCoordinate.longitude)
-        let destination = CLLocationCoordinate2DMake(destination.latitude, destination.longitude)
+        let destination = CLLocationCoordinate2DMake(coordinate?.latitude ?? 0, coordinate?.longitude ?? 0)
         
         let sourcePlacemark = MKPlacemark(coordinate: source)
         let destPlacemark = MKPlacemark(coordinate: destination)
-           
-        let finalSource = MKMapItem(placemark: sourcePlacemark)
-        let finalDest = MKMapItem(placemark: destPlacemark)
         
-        destCoordinate.source = finalSource
-        destCoordinate.destination =  finalDest
+        destCoordinate.source = MKMapItem(placemark: sourcePlacemark)
+        destCoordinate.destination =  MKMapItem(placemark: destPlacemark)
         
         
-               
+        let direction = MKDirections(request: destCoordinate)
+        direction.calculate{
+            (response, error) in
+               guard let locResponse = response else{
+                          if let error = error{
+                              print(error)
+                          }
+                          return
+                    }
+                for route in locResponse.routes{
+                       self.mapView.addOverlay(route.polyline,level:.aboveRoads)
+                       self.mapView.setVisibleMapRect(route.polyline.boundingMapRect, animated: true)
+                   }
+                  
+               }
+        
+         switch segTransportType.selectedSegmentIndex {
+                case 0 :
+                    destCoordinate.transportType = .walking
+                    for overlay in mapView.overlays{
+                        mapView.removeOverlay(overlay)
+                    }
+                case 1 :
+                    destCoordinate.transportType = .automobile
+                    for overlay in mapView.overlays{
+                        mapView.removeOverlay(overlay)
+                        
+                    }
+                default:
+                    break
+                }
+       
     }
    
 }
+//
+//  extension MKMapView {
+//    func initialLocation(
+//       _ location: CLLocation,
+//       regionRadius: CLLocationDistance = 1000
+//     ) {
+//       let coordinateRegion = MKCoordinateRegion(
+//         center: location.coordinate,
+//         latitudinalMeters: regionRadius,
+//
+//         longitudinalMeters: regionRadius)
+//       setRegion(coordinateRegion, animated: true)
+//     }
+//}
 
-  extension MKMapView {
-    func initialLocation(
-       _ location: CLLocation,
-       regionRadius: CLLocationDistance = 1000
-     ) {
-       let coordinateRegion = MKCoordinateRegion(
-         center: location.coordinate,
-         latitudinalMeters: regionRadius,
-         
-         longitudinalMeters: regionRadius)
-       setRegion(coordinateRegion, animated: true)
+extension ViewController: CLLocationManagerDelegate {
+     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last! as CLLocation
+        let currentLocation = location.coordinate
+        let coordinateRegion = MKCoordinateRegion(center: currentLocation, latitudinalMeters: 800, longitudinalMeters: 800)
+        mapView.setRegion(coordinateRegion, animated: true)
+        locationManager.stopUpdatingLocation()
+     }
+     
+     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error.localizedDescription)
      }
 }
